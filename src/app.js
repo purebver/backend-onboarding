3571441fa4204cb85781c42fa3e43cfde583437f
purebver <wkddj1541@gmail.com>
2025-02-15 17:01:37 +0900

    Feat: 메인 app.js에 라우터 추가
    sign라우터 추가

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import SignRouter from "./routes/sign.router.js";
 
 dotenv.config();
 
@@ -8,9 +9,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("백엔드 온보딩 과제");
-});
+app.get("/", [SignRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열림");
